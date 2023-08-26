cd406a3b56aa9a94c17b9f48c7ef2db701b4eb0f
IEA4 <lcdheir@mail.ru>
2023-08-26 09:00:01 +0300

    Oптимизировал массив оборотов и времени

--- PID_motor_prog_LED_LCD1608I2C/PID_motor_prog_LED_LCD1608I2C.ino
+++ PID_motor_prog_LED_LCD1608I2C/PID_motor_prog_LED_LCD1608I2C.ino
@@ -1,12 +1,18 @@
 //ПИД-регулирование оборотов двигателя с программной и ручной регулировкой и отображением производимых действий на светодиодах и ЖК-экране
 
+/*
+Не забудьте прописать в коде в строке Timer1.setPeriod(125000);  результат деления 1 000 000  на количество полюсов на кольцевом магните  
+При необходимости изменить максимальное количество оборотов вала (вместо 100 нужное значение) --> ...
+                                           ... 265 строка кода:  pid.setpoint = map(filt_pot.filteredTime(analogReadFast(POTEN_PIN)), 0, 1024, 0, 100);
+*/
+
 #define ZERO_PIN 2          // пин детектора ноля
-#define HOLL_PIN 3          // пин для датчика Холла 
+#define HOLL_PIN 3          // пин для датчика Холла
 #define DIMMER_PIN 4        // управляющий пин симистора
 #define BUTTON_PIN 5        // пин с кнопкой "ПУСК": запуск выбранного режима, запуск после переворота
 #define REGIM_PIN  7        // пин с тублером выбора регулирования: программный (Пр) / ручной (Руч)
 
-#define led_REG 6           // пин светодиода регулятора: повороту регулятора в сторону увеличения соответствует рост яркости светодиода  
+#define led_REG 6           // пин светодиода регулятора: повороту регулятора в сторону увеличения соответствует рост яркости светодиода
 #define LED_P 8             //            ... режима программного ...
 #define LED_R 9             //                   ... ручного регулирования
 
@@ -14,18 +20,20 @@
 
 #define period 1            // период расчетов и изменений (мс) в ПИД-регуляторе
 
-const unsigned int time_stup_arr[3][12] PROGMEM = {         // время работы ступеней  (мс)         // 3 программы по 12 ступеней 
-  {3000,    3000,   7500,   0,    7500,   7500,   8000,   10000,    0,    7500,   15000,  20000},   // отсчёт времени работы ступени начинается после того,
-  {3000,    5000,   10000,  0,    5000,   10000,  10000,  15000,    0,    10000,  20000,  25000},   //      как оборты вала выходят на обороты ступени 
-  {10000,   15000,  20000,  0,    10000,  15000,  20000,  25000,    0,    10000,  10000,  25000},
+/* при других величинах элементов массива нужен соответсвующий тип переменных,
+ а также соответствующия функция чтения переменной https://alexgyver.ru/lessons/progmem/ */
+const uint8_t time_stup_arr[3][12] PROGMEM = {         // время работы ступеней  (с)         // 3 программы по 12 ступеней
+  {90, 90, 90, 0, 60, 90, 90, 90, 0, 30, 60, 90},   // отсчёт времени работы ступени начинается после того,
+  {60, 120, 180, 0, 120, 120, 180, 240, 0, 60,  120, 120}, //   ... как обороты вала выходят на обороты ступени
+  {120, 180, 180, 0, 120, 120, 240, 240, 0, 120, 120, 120}, 
 };
-const unsigned int stup_arr[3][12] PROGMEM = {              // значения оборотов ступеней (оборотов в секунду вала электродвигателя)
-  {5, 15, 20, 0, 10, 20, 35, 45, 0, 10, 30, 45},
-  {5, 10, 15, 0, 10, 20, 35, 40, 0, 10, 25, 40},
-  {5, 10, 15, 0, 5,  10, 20, 30, 0, 10, 20, 30},
+const uint8_t stup_arr[3][12] PROGMEM = {              // значения оборотов ступеней (оборотов в секунду вала электродвигателя)
+  {20, 25, 35, 0, 20, 35, 45, 50, 0, 25, 40, 50},
+  {20, 25, 35, 0, 20, 35, 45, 50, 0, 25, 40, 50},
+  {20, 25, 35, 0, 20, 35, 45, 50, 0, 25, 40, 50},
 };
 
-#include "GyverPID.h"                   // библиотека ПИД-регулятора      // подробнее о библиотеке https://alexgyver.ru/gyverpid/ 
+#include "GyverPID.h"                   // библиотека ПИД-регулятора      // подробнее о библиотеке https://alexgyver.ru/gyverpid/
 GyverPID pid (30, 45, 0.1);             // назначение коэфов Kp, Ki, Kd   // подробнее о коэфах https://alexgyver.ru/lessons/pid/
 
 #include <FastDefFunc.h>                // библиотека для убыстрения функций: pinMode, digitalWrite, ...Read, analogWrite, ...Read
@@ -42,7 +50,7 @@ LiquidCrystal_I2C lcd(0x27,16,2);       //указываем адрес дисп
 VButton btn;                            //класс для кнопки
 
 boolean f_AR = 1;                       // флажок смены режима: Auto/Rutsch
-boolean f_AuL = 1;                     // флажок разрешения/запрета шаблонной информации
+boolean f_AuL = 1;                      // флажок разрешения/запрета шаблонной информации
 
 unsigned long tmr_pid;                  // для отсчёта времени при вычислениях в ПИД
 unsigned long tmr_lcd;                  //                    ... вывода новых данных на LCD
@@ -56,12 +64,12 @@ volatile unsigned int lastDim;          //                 ...предыдуща
 
 volatile unsigned int HOLL_N;           // для подсчёта кол-ва импульсов с датчика Холла
 
-volatile unsigned int odo;              // одометр -- кол-во оборотов в cекунду  
+volatile unsigned int odo;              // одометр -- кол-во оборотов в cекунду
 unsigned int odo_val;                   // "обычная" переменная, для переписывания данных с odo
 
 void setup() {
     Serial.begin(115200);                // скорость взаимодействи по Serial, 9600 -- слишком мало (в мониторе порта также должна быть выставлена эта скорость взаимодействия)
-    Serial.flush();                      // ждём окончания передачи предыдущих данных 
+    Serial.flush();                      // ждём окончания передачи предыдущих данных
 
     pinModeFast(HOLL_PIN, INPUT_PULLUP);  // INPUT_PULLUP -- один контакт на пин другой на GND, в выключенном состоянии на пине сигнал -- 1
     pinModeFast(BUTTON_PIN, INPUT_PULLUP);
@@ -78,7 +86,7 @@ void setup() {
     Timer1.enableISR(CHANNEL_B);          // подкл-но стандартное прерывание, канал B, без сдига фаз, частота ШИМ изменена на D10
     Timer2.enableISR(CHANNEL_A);          //                                                                              ... D11
 
-    attachInterrupt(digitalPinToInterrupt(ZERO_PIN), isr, RISING);    // функция прерывания вызывается по смене сигнала с 0 на 1 
+    attachInterrupt(digitalPinToInterrupt(ZERO_PIN), isr, RISING);    // функция прерывания вызывается по смене сигнала с 0 на 1
     attachInterrupt(digitalPinToInterrupt(HOLL_PIN), holl, FALLING);  // реагирует при смене сигнала c 1 на 0
 
     lcd.init();                           //инициализация дисплея
@@ -96,40 +104,40 @@ void loop(){
     while(true){
         while(digitalReadFast(REGIM_PIN) == 1){           // РЕЖИМ ПРОГРАММНОЙ РЕГУЛИРОВКИ. Откачка согласно программе, по ступенькам по заданным интервалам времени
 
-            lcd.setCursor(0,0);    lcd.print(F("P_"));      // Программный
+            lcd.setCursor(0,0);    lcd.print(F("p"));      // Программный
             lcd.setCursor(0,1);    lcd.print(F("Set:"));    // Выставлено
             lcd.setCursor(9,1);    lcd.print(F("Inp:"));    // Текущие
 
-            digitalWriteFast(LED_P, HIGH);          // сигнализируем включением светодиода-индикатора режима программной регулировки 
+            digitalWriteFast(LED_P, HIGH);          // сигнализируем включением светодиода-индикатора режима программной регулировки
 
             while (push_button() == 0){                   // ждать пока не нажмётся кнопка для выбора этого режима
                 if(digitalReadFast(REGIM_PIN) == 0){        // если при этом сменится режим работы,
                     f_AR = 0;                                     // кладём флажок режима
                     break;                                            // и выходим из программного режима
                 }
-                if (analogReadFast(POTEN_PIN) <= 341){        // во время ожидания регулятором РЕГ выбираем программу   
+                if (analogReadFast(POTEN_PIN) <= 341){        // во время ожидания регулятором РЕГ выбираем программу
                     j=0;
-                    lcd.setCursor(2,0); lcd.print(F("1"));
+                    lcd.setCursor(1,0); lcd.print(F("1"));
                 }
                 else if (analogReadFast(POTEN_PIN) > 341 && analogReadFast(POTEN_PIN) <= 682){
                     j = 1;
-                    lcd.setCursor(2,0); lcd.print(F("2"));
+                    lcd.setCursor(1,0); lcd.print(F("2"));
                 }
                 else if (analogReadFast(POTEN_PIN) > 682){
                     j = 2;
-                    lcd.setCursor(2,0); lcd.print(F("3"));
+                    lcd.setCursor(1,0); lcd.print(F("3"));
                 }
-                //Serial.println(analogReadFast(A6));         // использовать если нужно уст-ть другие диапазаны типа откачки (смотреть в мониторе порта)
+                //Serial.println(analogReadFast(POTEN_PIN));         // использовать если нужно уст-ть другие диапазаны типа откачки (смотреть в мониторе порта)
 
-                analogWriteFast(led_REG, analogReadFast(A6) >> 2);  // ШИМирование яркости светодиода регулятора в зависимости от величины его поворота
+                analogWriteFast(led_REG, analogReadFast(POTEN_PIN) >> 2);  // ШИМирование яркости светодиода регулятора в зависимости от величины его поворота
             }
 
             if (f_AR == 1)  auto_regim();            // если кнопка нажалась и не было смены режима работы  // производится откачка согласно выбранной программе
-            else      f_AR = 1;                      // если же был сменён режим, перед выходом поднимаем флажок режима  
-            
+            else      f_AR = 1;                      // если же был сменён режим, перед выходом поднимаем флажок режима
+
             lcd.clear();                             // очищаем экран
             digitalWriteFast(LED_P, LOW);            // выключаем светодиод - индикатор этого режима
-            digitalWriteFast(led_REG, LOW);          // выключаем светодиод - индикатор регулировки 
+            digitalWriteFast(led_REG, LOW);          // выключаем светодиод - индикатор регулировки
         }
 
         while (digitalReadFast(REGIM_PIN) == 0){         // РЕЖИМ РУЧНОЙ РЕГУЛИРОВКИ. Скорость откачки выставляется потенциометром (РЕГ)
@@ -154,7 +162,7 @@ void loop(){
                             digitalWriteFast(led_REG, !digitalReadFast(led_REG));      //мигаем светодиодом режима ручной регулировки
                             tmr_mig = millis();
                         }
-                    }                     
+                    }
                     lcd.setCursor(5,0);      lcd.print(F("          "));           // удаление просьбы "Pot to 0"
                 }
                 lcd.setCursor(4,1);   lcd.print(F("0"));                  // после нажатия на кнопку выводим нули
@@ -165,7 +173,7 @@ void loop(){
                 }
             }
             else   f_AR = 1;
-            
+
             lcd.clear();
             digitalWriteFast(LED_R, LOW);          // выключаем светодиод - индика
         }
@@ -174,11 +182,11 @@ void loop(){
 
 void auto_regim(){
     boolean f_AbO = 1;                     // флажок начала отсчёта откачки по периоду ступени
-    pid.integral = 9500;                   // при таком значении интегральной суммы время диммирования dimmer максимально, что необ. для минимума открытия симистора 
-    byte typ = analogReadFast(A6) >> 2;    // запоминаем яркость светодиода при выбранной программе 
-    for (i=0; i<12; i++){                                // 12 ступеней откачки
-        lcd.setCursor(4,0);     lcd.print(F("12|"));
-        lcd.setCursor(7,0);     lcd.print(i+1);                 // выводим на первой строчке номер ступени, начиная от 1
+    pid.integral = 9500;                   // при таком значении интегральной суммы время диммирования dimmer максимально, что необ. для минимума открытия симистора
+    byte typ = analogReadFast(POTEN_PIN) >> 2;    // запоминаем яркость светодиода при выбранной программе
+    for (i = 0; i < 12; i++){                                // 12 ступеней откачки
+        lcd.setCursor(3,0);     lcd.print(F("12|"));
+        lcd.setCursor(6,0);     lcd.print(i+1);                 // выводим на первой строчке номер ступени, начиная от 1
         lcd.setCursor(12,0);    lcd.print(F("|"));              // для таймера отсчёта
 
         if (i == 3 || i == 8){                      // на этих ступенях откачка останавливается для возможности перевернуть рамки
@@ -194,24 +202,29 @@ void auto_regim(){
                 //serial_print();               //плоттер
             }
             f_AuL = 1;               //добро на обновление данных для ступени
-            lcd.setCursor(9,0);    lcd.print(F("       "));
-            analogWriteFast(led_REG, typ);      // зажигаем светодиод регулятора 
+            lcd.setCursor(9,0);    lcd.print(F("       ")); // удаляем "POVOROT"
+            analogWriteFast(led_REG, typ);      // зажигаем светодиод регулятора
         }
 
         else if (i != 3 || i != 8){
-            uint16_t tmr_delta, tmr_deltaLast;
-            uint32_t tmrbO = millis();
+            uint8_t tmr_delta, tmr_deltaLast;
+            unsigned long tmrbO = millis();
 
-            lcd.setCursor(13,0);    lcd.print((unsigned int)(0.001 * (pgm_read_word(&time_stup_arr[j][i]))));   //обратный отсчт на дисплее
-            if  (pgm_read_word(&time_stup_arr[j][i]) < 10000){
-                lcd.setCursor(11,0);    lcd.print((unsigned int)(0.001 * (pgm_read_word(&time_stup_arr[j][i]))));
+            if  (pgm_read_byte(&time_stup_arr[j][i]) < 10){
+                lcd.setCursor(11,0);    lcd.print(pgm_read_byte(&time_stup_arr[j][i]));
             }
-            else {
-                lcd.setCursor(10,0);    lcd.print((unsigned int)(0.001 * (pgm_read_word(&time_stup_arr[j][i]))));
+            else if (pgm_read_byte(&time_stup_arr[j][i]) >= 10 && pgm_read_byte(&time_stup_arr[j][i]) < 100) {
+                lcd.setCursor(10,0);    lcd.print(pgm_read_byte(&time_stup_arr[j][i]));
             }
-            lcd.setCursor(15,0);    lcd.print(F(" "));   //костыль чтоб не выводились левые символы
+            else if (pgm_read_byte(&time_stup_arr[j][i]) >= 100) {
+                lcd.setCursor(9,0);     lcd.print(pgm_read_byte(&time_stup_arr[j][i]));
+            }
 
-            while(millis() - tmrbO < pgm_read_word(&time_stup_arr[j][i])){
+            lcd.setCursor(13,0);    lcd.print(pgm_read_byte(&time_stup_arr[j][i]));   //обратный отсчёт на дисплее
+            //lcd.setCursor(15,0);    lcd.print(F(" "));  //костыль чтоб не выводились левые символы
+
+            unsigned long tao = 1000 * ((uint32_t)pgm_read_byte(&time_stup_arr[j][i]));  //перевод в миллисекунды
+            while(millis() - tmrbO < tao){
                 pid_in_auto();                //пид-регулирование
                 //serial_print();               //плоттер
 
@@ -223,10 +236,13 @@ void auto_regim(){
                 }
 
                 if(f_AbO == 0){         //обратный отсчёт оставшегося времени откачки
-                    tmr_delta = (unsigned int)(0.001 * ((pgm_read_word(&time_stup_arr[j][i])) - (millis() - tmrbO)));
+                    tmr_delta = (uint8_t)(0.001 * (tao - (millis() - tmrbO)));
                     if (tmr_delta != tmr_deltaLast && tmr_delta >= 1){
                       lcd.setCursor(13,0);    lcd.print(tmr_delta);
-                      if (tmr_delta < 10){
+                      if (tmr_delta < 100 && tmr_delta >= 10){
+                         lcd.setCursor(15,0);    lcd.print(F(" "));
+                      }
+                      else if (tmr_delta < 10){
                          lcd.setCursor(14,0);    lcd.print(F("  "));
                       }
                       tmr_deltaLast = tmr_delta;
@@ -252,16 +268,16 @@ void rutsch_regim(){
             dimmer = int(expRAA(pid.output));                           //на управляющее устройство даётся расчитанный сигнал
             tmr_pid = millis();
         }
-      
-        analogWriteFast(led_REG, analogReadFast(A6) >> 2);  // яркость светодиода регулировки в соответсвии с поворотом ручки регулятора оборотов 
-        
+
+        analogWriteFast(led_REG, analogReadFast(POTEN_PIN) >> 2);  // яркость светодиода регулировки в соответсвии с поворотом ручки регулятора оборотов
+
         if (millis() - tmr_lcd >= 500){                                  //вывод новых данных каждые 500мс
             lcd.setCursor(4,1);   lcd.print(int(pid.setpoint));         //выставленные на потенциометре обороты
             lcd.setCursor(13,1);   lcd.print(int(pid.input));           //текущие обороты двигателя
             if (int(pid.setpoint) < 10){                                //если выставленные меньше 10, то очищаем на дисплее место второй цифры (для ясности выводимой информации)
                 lcd.setCursor(5,1);   lcd.print(F("  "));
             }
-            
+
             if ( f_z == 0 && int(pid.input) < 10){
                 lcd.setCursor(14,1);   lcd.print(F("  "));
                 f_z = 1;
@@ -274,7 +290,7 @@ void rutsch_regim(){
               }
             }
             else if (int(pid.input) >= 100) f_h = 0;
-                     
+
             tmr_lcd = millis();
         }
 
@@ -287,8 +303,8 @@ void rutsch_regim(){
 //функция пид-регулирования в программном режиме
 void pid_in_auto(){
     if (f_AuL == 1){
-        lcd.setCursor(4,1);    lcd.print(pgm_read_word(&stup_arr[j][i]));
-        pid.setpoint = pgm_read_word(&stup_arr[j][i]);   // берём значения необходимых оборотов из массива
+        lcd.setCursor(4,1);    lcd.print(pgm_read_byte(&stup_arr[j][i]));
+        pid.setpoint = (int)(pgm_read_byte(&stup_arr[j][i]));   // берём значения необходимых оборотов из массива
         f_AuL = 0;
     }
 
